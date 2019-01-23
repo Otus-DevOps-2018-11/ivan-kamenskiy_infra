91d7093ddb51d683923a13b8089e3683c18abceb
Ivan Kamenskiy <narcotik123@mail.ru>
2019-01-23 14:55:30 +0300

    Add resoure firewall and app_ip

--- terraform/main.tf
+++ terraform/main.tf
@@ -61,3 +61,15 @@ resource "google_compute_firewall" "firewall_puma" {
   # Правило применимо для инстансов с перечисленными тэгами
   target_tags = ["reddit-app"]
 }
+resource "google_compute_firewall" "firewall_ssh" {
+name = "default-allow-ssh"
+network = "default"
+allow {
+protocol = "tcp"
+ports = ["22"]
+}
+source_ranges = ["0.0.0.0/0"]
+}
+resource "google_compute_address" "app_ip" {
+name = "reddit-app-ip"
+}
